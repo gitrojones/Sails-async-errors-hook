1bb0b6d1c8c8324a4537e42c11d035d020d8d978
Ryann Jones <ryann@rojones.us>
2017-12-04 09:57:30 +0000

    Fixed symbolic links. Woops

--- api/hooks/asyncErrorHook.js
+++ api/hooks/asyncErrorHook.js
@@ -1,1 +1,91 @@
-/home/rojones/.projects/meanvapes/back/api/hooks/asyncErrorHook.js
+let fs = require('fs')
+let util = require('util')
+
+let asyncErrorHandler = require('../middleware/asyncErrorHandler')
+
+module.exports = function asyncErrorHook (sails) {
+  return {
+    async initialize (cb) {
+      // Gather and 'register' all errors within /api/errors
+      sails.errors = await hoistedErrorDirectory()
+
+      sails.on('router:before', function () {
+        let routes = sails.router.explicitRoutes
+
+        for (let key in routes) {
+          let controller = routes[key].replace('.', '/').replace('Controller', '').toLowerCase()
+          let action = sails._actions[controller]
+
+          if (typeof action !== 'function') continue
+
+          let middleware = action['_middlewareType']
+          sails._actions[controller] = asyncErrorHandler(action, controller)
+          sails._actions[controller]['_middlewareType'] = middleware
+        }
+
+      })
+
+      return cb()
+    }
+  }
+}
+
+async function hoistedErrorDirectory() {
+  let errorDir = sails.config.appPath + '/api/errors'
+  let errorFileNames
+
+  try {
+    errorFileNames = await new Promise((resolve, reject) => fs.readdir(errorDir, (err, files) => {
+      if (err) reject(err)
+      return resolve(files)
+    }))
+  } catch (err) {
+    sails.log.error('Failed to gather files from /api/errors. Details\n\n' + err)
+  }
+
+  if (_.isArray(errorFileNames)) {
+    // We have files to process
+    sails.log.silly('Hoisting the following files:', errorFileNames)
+    let errors = {}
+
+    for (let file of errorFileNames) {
+      if (!file.match(/.+Exception\.js/)) continue
+      let filePath = errorDir + '/' + file
+      let data
+
+      try {
+        data = eval(
+          await new Promise((resolve, reject) => fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) reject(err)
+            return resolve(data)
+          }))
+        )
+      } catch (err) {
+        sails.log.error(`Failed to load error file ${file}. Details:\n\n${err}`)
+      }
+
+      if (_.has(data, 'name')) {
+        errors[data.name] = ErrorWrapper(data)
+        sails.log.silly('Successfully loaded custom error', file)
+      } else sails.log.error('Expected exception to define "name". Skipping', file)
+    }
+
+    sails.log.verbose('Registered the following custom exceptions:', errors)
+    return errors
+  } else sails.log.error('No errors to hoist. Ensure the /api/errors directory exists.',
+    'See documentation for more details.')
+}
+
+function ErrorWrapper({name, code, details}) {
+  // Basically our errors define common details and we extend Error with the new details.
+  let CustomError = function () {
+    Error.captureStackTrace(this, CustomError);
+    this.name = name;
+    this.code = code;
+    this.details = details;
+  }
+
+  util.inherits(CustomError, Error)
+
+  return CustomError
+}
--- api/middleware/asyncErrorHandler.js
+++ api/middleware/asyncErrorHandler.js
@@ -1,1 +1,18 @@
-/home/rojones/.projects/meanvapes/back/api/middleware/asyncErrorHandler.js
+module.exports = function asyncErrorHandler (action, controller) {
+  sails.log.verbose('Initialized asyncErrorHandler (HTTP Middleware).\n\tController (' +
+                  controller + ') errors are being handled.')
+
+  return async function (req, res, next) {
+    let caller = `${req.method} @ ${req.url} invoking ${controller}`
+
+    if (typeof action !== 'function') res.serverError('Wrapper expected action to be a function.')
+    sails.log.silly('Handling HTTP request: ' + caller);
+
+    try {
+      await action(req, res, next)
+    } catch (err) {
+      sails.log.error(`Handling encountered error ${err.code}.`)
+      return sails.helpers.errorHandling({caller, err, res}).execSync()
+    }
+  }
+}
